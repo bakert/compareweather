9788dbe602e93ce177f60e33394c76be9e8dab18
Thomas David Baker <bakert@gmail.com>
2013-09-25 13:47:30 +0100

    Basic framework.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ compareweather
 ==============
 
 Compare the weather in two locations. Minimalist web app.
+
+Setup
+=====
+
+	* Register with developer.forecast.io
+	* Copy/move config.sample to config
+	* Add your forecast.io API key in place of <APIKEY>
+
---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+
+// Configuration information loaded from a file.
+class Config {
+    const CONFIG_FILE = 'config';
+    const DIVIDER = ' = ';
+
+    protected static $config;
+
+    protected static function init() {
+        $path = dirname(__FILE__) . '/' . static::CONFIG_FILE;
+        if (!file_exists($path)) {
+            throw new Exception('Configuration file not found at ' . $path);
+        }
+        static::$config = [];
+        $rawConfig = file_get_contents($path);
+        foreach (explode("\n", $rawConfig) as $line) {
+            if (strpos($line, static::DIVIDER) === false) {
+                continue;
+            }
+            list($key, $value) = explode(static::DIVIDER, $line);
+            static::$config[$key] = $value;
+        }
+    }
+
+    public static function __callStatic($name, array $args) {
+        if (!isset(static::$config)) {
+            static::init();
+        }
+        if (isset(static::$config[$name])) {
+            return static::$config[$name];
+        } else {
+            return null;
+        }
+    }
+}
+
