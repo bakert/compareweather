58576ef78b46eb29afe2a019765688809c2ac4ad
Thomas David Baker <bakert@gmail.com>
2013-10-09 20:55:27 +0100

    Autoload third party code, provide a global html escape function.

--- compareweather.php
+++ compareweather.php
@@ -1,4 +1,10 @@
 <?php
 
-require_once('config.php');
+require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/config.php');
 
+date_default_timezone_set('UTC');
+
+function q($s) {
+	return htmlentities($s);
+}
